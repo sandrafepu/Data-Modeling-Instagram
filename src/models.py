115f06f30c6b0fd23f7752e9f1ddb6355c284a08
sandrafepu <sandrafepu@gmail.com>
2023-03-14 10:24:12 +0000

    Arreglo issue

--- src/models.py
+++ src/models.py
@@ -10,7 +10,6 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-
     id = Column(Integer, primary_key=True)
     name = Column(String(20), nullable=False)
     firstname = Column(String(50), nullable=False)
@@ -18,41 +17,40 @@ class User(Base):
     password = Column(String(20), nullable=False)
     email = Column(String(20), nullable=False)
 
+
 class Post(Base):
     __tablename__ = 'post'
-   
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user_id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+
 
 class Media(Base):
     __tablename__ = 'media'
-   
     id = Column(Integer, primary_key=True)
-    type = Column(String(50), nullable=False)
+    media_type = Column(String(50), nullable=False)
     url = Column(String(100), nullable=False)
-    post_id = Column(Integer, ForeignKey('post_id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
     post = relationship(Post)
+
 
 class Comment(Base):
     __tablename__ = "comment"
-
     id = Column(Integer, primary_key = True)
-    comment_text = Column(String(250))
-    author_id = Column(Integer, ForeignKey("user.id"))
-    post_id = Column(Integer, ForeignKey("post.id"))
+    comment_text = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
     user = relationship(User)
     post = relationship(Post)
 
 class Followers(Base):
     __tablename__ = 'followers'
-
     id = Column(Integer, primary_key = True)
-    user_from_id = Column(Integer, ForeignKey("user.id")) 
-    user_to_id = Column(Integer, ForeignKey("user.id"))     
-    user = relationship(User)
+    user_from_id = Column(Integer, ForeignKey('user.id')) 
+    user_to_id = Column(Integer, ForeignKey('user.id'))     
+    user_from = relationship(User)
+    user_to = relationship(User)
     
-
 
     def to_dict(self):
         return {}
